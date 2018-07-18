58b59c45e3015127e7cc1087b285b88b802adf68
wenbobao <744865006@qq.com>
2018-07-18 11:00:32 +0800

    update root path

--- demo/config/index.js
+++ demo/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'learn-vue2/demo/dist/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/learn-vue2/demo/dist/',
 
     /**
      * Source Maps
